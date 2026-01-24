37ba20a4a76eb617de192e81ae0f27b98e9299b6
Brian Carey <brian.carey@protonmail.com>
2026-01-24 19:34:37 +0000

    build: update the bump of distroless base image to also update container builds
    There is automation in place to bump the distroless base image used for the bazel built images - update this bump script to also bump the base image used by the podman/docker built image flow
    
    Signed-off-by: Brian Carey <brian.carey@protonmail.com>

--- hack/bump-distroless.sh
+++ hack/bump-distroless.sh
@@ -6,9 +6,9 @@ source hack/common.sh
 source hack/bootstrap.sh
 source hack/config.sh
 
-DISTROLESS_AMD64_DIGEST=$(skopeo inspect docker://gcr.io/distroless/base:latest-amd64 | jq '.Digest' -r)
-DISTROLESS_ARM64_DIGEST=$(skopeo inspect docker://gcr.io/distroless/base:latest-arm64 | jq '.Digest' -r)
-DISTROLESS_S390X_DIGEST=$(skopeo inspect docker://gcr.io/distroless/base:latest-s390x | jq '.Digest' -r)
+DISTROLESS_AMD64_DIGEST=$(skopeo inspect docker://gcr.io/distroless/base-debian12:latest-amd64 | jq '.Digest' -r)
+DISTROLESS_ARM64_DIGEST=$(skopeo inspect docker://gcr.io/distroless/base-debian12:latest-arm64 | jq '.Digest' -r)
+DISTROLESS_S390X_DIGEST=$(skopeo inspect docker://gcr.io/distroless/base-debian12:latest-s390x | jq '.Digest' -r)
 
 # buildozer returns a non-zero exit code (3) if the commands were a success but did not change the file.
 # To make the command idempotent, first set the digest to a kind-of-unique number to work around this behaviour.
@@ -23,3 +23,8 @@ set digest "${DISTROLESS_AMD64_DIGEST}"|${KUBEVIRT_DIR}/WORKSPACE:go_image_base
 set digest "${DISTROLESS_ARM64_DIGEST}"|${KUBEVIRT_DIR}/WORKSPACE:go_image_base_aarch64
 set digest "${DISTROLESS_S390X_DIGEST}"|${KUBEVIRT_DIR}/WORKSPACE:go_image_base_s390x
 EOF
+
+# Update hack/build-images-container.sh to keep digests in sync
+sed -i "/amd64)/,/;;/ s|DISTROLESS_DIGEST=\"sha256:[a-f0-9]*\"|DISTROLESS_DIGEST=\"${DISTROLESS_AMD64_DIGEST}\"|" hack/build-images-container.sh
+sed -i "/arm64)/,/;;/ s|DISTROLESS_DIGEST=\"sha256:[a-f0-9]*\"|DISTROLESS_DIGEST=\"${DISTROLESS_ARM64_DIGEST}\"|" hack/build-images-container.sh
+sed -i "/s390x)/,/;;/ s|DISTROLESS_DIGEST=\"sha256:[a-f0-9]*\"|DISTROLESS_DIGEST=\"${DISTROLESS_S390X_DIGEST}\"|" hack/build-images-container.sh
